e7147ebb467d10172ff07fd7ef6badf91e57084c
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2023-11-16 19:18:03 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.dayone;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		int value1=11/3;            //integer division
+		float value2=235/61f;
+		double value3=14d/8d;
+		float value4=7/2.5f;        //float division
+		
+		System.out.println("value 1="+value1);
+		System.out.println("value 2="+value2);
+		System.out.println("value 3="+value3);
+		System.out.println("value 4="+value4);
+		
+		int wrap=512;
+		                    //assigning expression to variable
+		double percentage=wrap*0.46f;
+		System.out.println("percentage :"+percentage);
+
+	}
+
+}
