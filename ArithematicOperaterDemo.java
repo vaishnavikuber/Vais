ac3c7786ac8637d96cef96984b6d5109d981d5a3
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2023-11-17 19:31:00 +0530

    Add files via upload

---
+++ ArithematicOperaterDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayone;
+
+public class ArithematicOperaterDemo {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 3;
+       
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+	}
+
+}
