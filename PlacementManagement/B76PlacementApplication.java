86353ffefbcea3d264320b0235e9367ad93fc841
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2024-03-15 11:56:13 +0530

    initial commit

---
+++ PlacementManagement/B76PlacementApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B76PlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B76PlacementApplication.class, args);
+	}
+
+}
